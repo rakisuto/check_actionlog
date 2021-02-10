ef4bc7ac5faac4efa87523c760e4cd1368470372
rakisuto <tama5.bo6bo6@gmail.com>
2021-02-10 10:02:16 +0900

    add css

--- app.py
+++ app.py
@@ -42,15 +42,14 @@ def verify_password(pwd, hash):
 # ログイン画面
 @app.route('/')
 def main():
-    props = {'title': 'Practice', 'msg': 'ユーザ登録＆ログイン'}
-    html = render_template('index.html', props=props)
-    return html
+    props = {'title': 'login or sign up', 'msg': 'ユーザ登録＆ログイン'}
+    return render_template('index.html', props=props)
 
 # ユーザ登録画面に遷移
-@app.route('/hello')
-def hello():
-    props = {'title': 'Practice - sign up', 'msg': 'Sign up'}
-    html = render_template('hello.html', props=props)
+@app.route('/regist_user')
+def regist_user():
+    props = {'title': 'sign up', 'msg': 'ユーザ登録'}
+    html = render_template('user_regist.html', props=props)
     return html
 
 # ユーザー登録
@@ -65,9 +64,11 @@ def register():
     # ユーザーを追加
     add_result = add_user(name, age, gender, hash_pwd)
     if add_result:
-        return '<h1>登録に成功</h1><a href="/">戻る</a>'
+        props = {'title': 'success sign up', 'msg': 'ユーザを登録しました！'}
+        return render_template('msg.html', props=props)
     else:
-        return '<h1>登録に失敗</h1><a href="/">戻る</a>'
+        props = {'title': 'failed sign up', 'msg': 'ユーザの登録に失敗しました。'}
+        return render_template('msg.html', props=props)
 
 # DBにユーザ追加
 def add_user(name, age, gender, pwd):
@@ -82,7 +83,7 @@ def add_user(name, age, gender, pwd):
 # ログイン画面に遷移
 @app.route('/login')
 def login():
-    props = {'title': 'Practice - sign in', 'msg': 'Sign in'}
+    props = {'title': 'try login', 'msg': 'ログイン試行'}
     return render_template('login_form.html', props=props)
 
 # ログイントライ
@@ -95,15 +96,15 @@ def login_try():
         session['login'] = name
         return redirect('/user/home')
     else:
-        return msg('ユーザ名かパスワードが誤っています。染色体からやり直して下さい。')
-
+        props = {'title': 'login failed', 'msg': 'ログインに失敗しました。ユーザ名かパスワードが誤っています'}
+        return render_template('msg.html', props=props)
 
 # ログイン成功→ユーザーHOME画面
 @app.route('/user/home')
 def login_success():
     name = get_name()
     # ログイン先の画面(user_home.html)に遷移さす。
-    props = {'title': 'user_home', 'msg':'ホーム画面です(仮)'}
+    props = {'title': 'user_home', 'msg':'あなたのマイページ'}
     return render_template('user_home.html', name=name, props=props)
 
 # DBからログイン情報参照 レコードのCOUNT結果で存在有無を判断する！
@@ -133,7 +134,8 @@ def get_name():
 @app.route('/logout')
 def logout():
     try_logout()
-    return msg('お疲れ様でした(⌒,_ゝ⌒)')
+    props = {'title': 'logout', 'msg': 'お疲れ様でした(⌒,_ゝ⌒)'}
+    return render_template('msg.html', props=props)
 
 # ログアウトトライ
 def try_logout():
@@ -155,7 +157,7 @@ def login_required(func):
 def check_old_log():
     name = get_name()
     dt = get_today()
-    props = {'title': 'user_old_log', 'msg': '過去ログ'}
+    props = {'title': 'user old log', 'msg': '過去の記録確認'}
     stmt = 'SELECT memo, dt, tm FROM users_log\
             WHERE name = ? AND dt = cast(now() as date)'
     timeline = db.query(stmt, name, prepared=True)
@@ -168,18 +170,19 @@ def check_old_log():
 def check_calendar():
     name = get_name()
     dt = request.form.get('date')
-    props = {'title': 'user_old_log', 'msg': '過去ログ'}
+    props = {'title': 'user old log', 'msg': '過去の記録確認'}
     stmt = 'SELECT memo, dt, tm FROM users_log\
             WHERE name = ? AND dt = ?'
     timeline = db.query(stmt, name, dt, prepared=True)
     return render_template('user_old_log.html',
             name=name, dt=dt, timeline=timeline, props=props)
 
+
 # 記録を作成する！
 @app.route('/user/add_log')
 @login_required
 def add_log():
-    props = {'title': 'user_add_log', 'msg': '記録の間'}
+    props = {'title': 'write log', 'msg': '今この瞬間の出来事・気持ちなどを記録しよう'}
     return render_template('user_add_log.html',
             name=get_name(), props=props)
 
@@ -190,7 +193,7 @@ def write_log():
     memo = request.form.get('text')
     dt = get_today()
     tm = get_time()
-    props = {'title': 'user_add_log', 'msg': '記録出来ました！この調子！'}
+    props = {'title': 'success write log', 'msg': '記録出来ました！この調子！'}
     stmt = 'INSERT INTO users_log values\
             (?, ?, ?, ?);'
     write_log = db.ins_query(stmt, name, memo, dt, tm, prepared=True)
@@ -209,48 +212,6 @@ def get_time():
     t = datetime.datetime.now()
     time = (t.strftime('%H:%M:%S'))
     return time
-
-# メッセージ出力
-def msg(msg):
-    return render_template('msg.html', msg=msg)
-
-# 改行有効フィルター
-# tupleとなっているsから、memo要素を取り出してやる。
-@app.template_filter('linebreak')
-def linebreak_filter(text):
-    # tupleの要素取り出し
-    s_list = list(text)
-    s_memo = s_list[0]
-    s_memo = s_memo.replace('&', '&amp;').replace('<', '&lt;') \
-            .replace('>', '&gt;').replace('\n', '<br>')
-    return Markup(s_memo)
-
-# 日付フォーマット
-# tupleのsから、date要素をとりだす。
-#@app.template_filter('datestr')
-#def datestr_filter(t):
-    # tupleの要素取り出し
-#    t_list = list(t)
-#    t_date = t_list[1]
-#    return time.strftime('%Y年%m月%d日',
-#    time.localtime(t_date))
-
-# 時間フォーマットも作る必要がある。、。
-# tupleのsから、date要素をとりだす。
-#@app.template_filter('datestr')
-#def datestr_fiter(t):
-    # tupleの要素取り出し
-#   t_list = list(t)
-#    return time.strftime('%Y年%m月%d日',
-#    time.localtime(t_list))
-
-# @app.route('/users/<int:id>')
-# def user(id):
-#    props = {'title': 'User Information', 'msg': 'User Information'}
-#    stmt = 'SELECT * FROM users WHERE id = ?'
-#    user = db.query(stmt, id, prepared=True)
-#    html = render_template('user.html', props=props,user=user[0])
-#    return html
 
 @app.errorhandler(404)
 def not_found(error):
